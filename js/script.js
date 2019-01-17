e02808568828e8bb8ee5cddf481a30831afbe842
Samuel Estevez <julioestevez@aclanyc.com>
2019-01-17 23:00:45 +0000

    a

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = ["aaa"];
+var songs = ["Sono chi no Sadame", "BLOODY STREAM", "Stand Proud", "Sono chi no kioku", "Crazy Noisy Bizarre Town", "Chase", "Great Days", "Fighting Gold"];
 // BELOW Add 4 More arrays to store images_links, atists, song lengths, and links for each song
 // Make sure they match the same order as your array above
-var artists = [];
-var imageLinks = [];
-var songLengths = [];
-var songLinks = [];
+var artists = ["", "Coda", "", "", "", "", "", "Coda"];
+var imageLinks = ["", "", "", "", "", "", "", ""];
+var songLengths = ["4:20", "", "", "", "", "", "", ""];
+var songLinks = ["https://www.youtube.com/watch?v=yI3pg_YJnPM", "", "", "", "", "", "", ""];
 
     // BELOW Use forEach Loop to display the data from each of your array's in the correct div
 
@@ -32,14 +32,14 @@ function addSongInfo(){
 }
 
 function displaySongInfo(){
-
-    for(let i = 0; i < songs.length; i++){
+    songs.forEach(function(song, i){
         $("#images").append("<img src='" + imageLinks[i] + "'>");
         $("#songs").append("<p>" + songs[i] + "</p>");
         $("#artists").append("<p>" + artists[i] + "</p>");
         $("#lengths").append("<p>" + songLengths[i] + "</p>");
         $("#links").append("<p>" + songLinks[i] + "</p>");
-    }
+    })
+        
 }
 
 $("#add").click(function() {
